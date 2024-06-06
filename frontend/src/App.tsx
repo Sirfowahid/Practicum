033093f83ea26ae8ebe41d7d671b9751f81c4df5
Sirfowahid <mehedii2912@gmail.com>
2024-06-06 12:26:44 +0600

    Add Tailwind CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-
 function App() {
-
 
   return (
     <>
-    <h1>Hello World!</h1>
+    <h1 className="text-3xl text-center py-3 text-white bg-slate-700 font-bold">
+      Tailwind CSS Added!
+    </h1>
+
     </>
   )
 }
