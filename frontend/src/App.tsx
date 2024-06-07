48e8cd3c0a05db002e03f7d5cb348a28af4130af
Sirfowahid <mehedii2912@gmail.com>
2024-06-07 20:49:50 +0600

    Adding Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
+import Navbar from './components/Navbar'
 import './App.css'
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl text-center py-3 text-white bg-slate-700 font-bold">
-      Tailwind CSS Added!
-    </h1>
-
+    <Navbar/>
     </>
   )
 }
