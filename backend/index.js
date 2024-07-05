84f937971e1c069d54c80e89f0e2b2f5e17f3c4b
Sirfowahid <mehedii2912@gmail.com>
2024-07-05 23:32:48 +0600

    Add error Middleware

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 import hotel from "./data/hotel.js";
-import products from "./data/products.js";
 
 const port = process.env.PORT || 5000;
 
@@ -37,5 +37,8 @@ app.get("/rooms/:roomId", (req, res) => {
     const room = hotel.find((r) => r.id === parseInt(req.params.roomId));
     res.json(room);
 });
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Port is running in ${port}`));
