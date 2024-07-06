a6a12baeec6646f22e0598b9f54673dcd06bc02d
Sirfowahid <mehedii2912@gmail.com>
2024-07-06 06:22:18 +0600

    add booking and user routes

---
+++ backend/data/userData.js
@@ -0,0 +1,19 @@
+const usersData = [
+    { id: 1, username: 'john_doe', email: 'john.doe@example.com', role: 'Admin' },
+    { id: 2, username: 'jane_smith', email: 'jane.smith@example.com', role: 'User' },
+    { id: 3, username: 'michael_johnson', email: 'michael.johnson@example.com', role: 'User' },
+    { id: 4, username: 'emily_davis', email: 'emily.davis@example.com', role: 'User' },
+    { id: 5, username: 'david_clark', email: 'david.clark@example.com', role: 'Admin' },
+    { id: 6, username: 'susan_baker', email: 'susan.baker@example.com', role: 'User' },
+    { id: 7, username: 'robert_jones', email: 'robert.jones@example.com', role: 'Admin' },
+    { id: 8, username: 'linda_anderson', email: 'linda.anderson@example.com', role: 'User' },
+    { id: 9, username: 'william_taylor', email: 'william.taylor@example.com', role: 'User' },
+    { id: 10, username: 'sarah_wilson', email: 'sarah.wilson@example.com', role: 'Admin' },
+    { id: 11, username: 'kevin_miller', email: 'kevin.miller@example.com', role: 'User' },
+    { id: 12, username: 'amanda_thomas', email: 'amanda.thomas@example.com', role: 'Admin' },
+    { id: 13, username: 'peter_young', email: 'peter.young@example.com', role: 'User' },
+    { id: 14, username: 'laura_moore', email: 'laura.moore@example.com', role: 'User' },
+    { id: 15, username: 'chris_wilson', email: 'chris.wilson@example.com', role: 'User' },
+  ];
+  
+  export default  usersData
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 import hotel from "./data/hotel.js";
+import bookings from "./data/bookingData.js"
+import users from "./data/bookingData.js"
 import roomsRouter from "./routes/roomsRoutes.js"
 const port = process.env.PORT || 5000;
 
@@ -39,6 +41,24 @@ app.get("/rooms/:roomId", (req, res) => {
     const room = hotel.find((r) => r.id === parseInt(req.params.roomId));
     res.json(room);
 });
+
+app.get("/users",(req,res)=>{
+    res.json(users)
+})
+
+app.get("/users/:userId",(req,res)=>{
+    const user = users.find((u)=>u.id ===parseInt(req.params.userId))
+    res.json(user)
+})
+
+app.get("/bookings",(req,res)=>{
+    res.json(bookings)
+})
+
+app.get("/bookings/:bookingId",(req,res)=>{
+    const booking = bookings.find((b)=>b.id ===parseInt(req.params.bookingId))
+    res.json(booking)
+})
 
 app.use(notFound)
 app.use(errorHandler)
