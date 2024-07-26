2a48abdc4e21c363f5763fc26024ae8f1120abbf
Sirfowahid <mehedii2912@gmail.com>
2024-07-26 21:06:38 +0600

    Add Auth Slice for frontend Authentication

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlice";
+import authReducer from '../slices/authSlice'
 const store = configureStore({
     reducer: {
-        [apiSlice.reducerPath]:apiSlice.reducer
+        [apiSlice.reducerPath]:apiSlice.reducer,
+        auth:authReducer
     },
     middleware:(getDefalutMiddleware) => getDefalutMiddleware().concat(apiSlice.middleware),
     devTools:true
